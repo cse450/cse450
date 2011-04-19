5b059d5f755ea9cef04b32ea735bf84246dae543
Nick <ad@aphrodite.(none)>
2011-04-18 21:35:11 -0400

    DOUBLE = 1

--- proj5/Atom.java
+++ proj5/Atom.java
@@ -1,6 +1,6 @@
 public class Atom {
   public static final int INTEGER = 0;
-  public static final int DOUBLE = 0;
+  public static final int DOUBLE = 1;
         
   public Number Value;
   public int Type;
